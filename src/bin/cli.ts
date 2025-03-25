cad6a74eb7f24855b78b889536e8f4f035daa258
Fabricio Futryk <ffutryk52@gmail.com>
2025-03-24 21:12:23 -0300

    feat: add cleye cli

---
+++ src/bin/cli.ts
@@ -0,0 +1,8 @@
+import { cli } from 'cleye';
+import { newCommand } from '../commands/new/new';
+
+const argv = cli({
+  name: 'express-prisma-cli',
+  version: '1.0.0',
+  commands: [newCommand],
+});
